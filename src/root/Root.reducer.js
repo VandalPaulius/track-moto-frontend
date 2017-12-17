a2159de715889a16e61b637261432d6c43409685
Vandal Paulius Juska <aki.maxis@gmail.com>
2017-12-17 16:31:23 +0000

    Fixed Root reducer errors

--- src/root/Root.reducer.js
+++ src/root/Root.reducer.js
@@ -1,27 +1,25 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { appReducer, panelReducer, mapReducer } from '../app';
-import * as constants from './Root.constants';
 
-// export default combineReducers({
-//   app: appReducer,
-//   form: formReducer,
-//   panel: panelReducer,
-//   map: mapReducer
-// });
+export default combineReducers({
+  app: appReducer,
+  form: formReducer,
+  panel: panelReducer,
+  map: mapReducer
+});
 
+// const rootReducer = (state, action) => {
+//   if (action.type === 'RESET_STATE') {
+//     return undefined; // state = undefined;
+//   }
 
-const rootReducer = (state, action) => {
-  if (action.type === 'RESET_STATE') {
-    return undefined; // state = undefined;
-  }
+//   return combineReducers({
+//     app: appReducer,
+//     form: formReducer,
+//     panel: panelReducer,
+//     map: mapReducer
+//   });
+// };
 
-  return combineReducers({
-    app: appReducer,
-    form: formReducer,
-    panel: panelReducer,
-    map: mapReducer
-  });
-};
-
-export default rootReducer;
+//export default rootReducer;
